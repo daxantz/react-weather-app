c009f9075c95d04c9826ae3178459b3ce975d6f8
daxantz <daxantz@gmail.com>
2024-12-21 19:48:57 -0500

    add router link to navigate to city pages

--- src/components/CityLink.tsx
+++ src/components/CityLink.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 type CityLinkProps = {
   country: string;
   name: string;
@@ -8,7 +10,7 @@ type CityLinkProps = {
 const CityLink = ({ country, name, temp, id }: CityLinkProps) => {
   return (
     <div className="flex justify-around items-center border uppercase">
-      <div className="flex items-center w-72 gap-3 pl-5 ">
+      <div className="flex items-center w-72 gap-3  ">
         <div className="rounded-md bg-slate-300 w-9 py-2 px-5 flex justify-center">
           🇫🇷
         </div>
@@ -17,9 +19,12 @@ const CityLink = ({ country, name, temp, id }: CityLinkProps) => {
           <span>{name}</span>
         </div>
       </div>
-      <div>
+      <div className="flex gap-5">
         <span>{temp}&#x2103;</span>
-        <span>&rarr;</span>
+        <Link to={`city/${id}`}>
+          {" "}
+          <span>&rarr;</span>
+        </Link>
       </div>
     </div>
   );
