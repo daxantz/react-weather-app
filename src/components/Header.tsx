1dd0a6544c3897a0e0b6cecb0f430289f7f16338
daxantz <daxantz@gmail.com>
2024-12-21 19:48:17 -0500

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="border border-green-400 p-4 flex gap-80">
+      <h1 className="text-5xl font-medium">
+        Here you can find a world <br /> wide weather forecast
+      </h1>
+      <p className="text-base flex-1 ">
+        Contented get distrusts certainty nay are frankness concealed ham. On
+        unaffected resolution on considered of. No thought me husband or colonel
+        forming effects. End sitting shewing who saw besides son musical
+        adapted. Contrasted interested eat alteration pianoforte sympathize was.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
