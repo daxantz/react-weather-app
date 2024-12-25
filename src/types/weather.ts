9e5020cd11b05b0400fb1fee61b0cfcf4a6579e4
daxantz <daxantz@gmail.com>
2024-12-25 16:22:50 -0500

    create type for weather object fetched from weather api

---
+++ src/types/weather.ts
@@ -0,0 +1,38 @@
+type Weather = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string; // e.g., "Rain"
+    description: string; // e.g., "light rain"
+    icon: string; // e.g., "10d"
+  }[];
+  main: {
+    temp: number; // Temperature in Kelvin
+    feels_like: number; // Feels-like temperature
+    temp_min: number; // Minimum temperature
+    temp_max: number; // Maximum temperature
+    pressure: number; // Atmospheric pressure
+    humidity: number; // Humidity percentage
+  };
+  wind: {
+    speed: number; // Wind speed
+    deg: number; // Wind direction
+  };
+  clouds: {
+    all: number; // Cloudiness percentage
+  };
+  sys: {
+    country: string; // Country code, e.g., "US"
+    sunrise: number; // Sunrise time (UNIX timestamp)
+    sunset: number; // Sunset time (UNIX timestamp)
+    id: number;
+  };
+  name: string; // City name
+  dt: number; // Data calculation time (UNIX timestamp)
+  id: number;
+};
+
+export default Weather;
