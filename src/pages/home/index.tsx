57dae7562ddb6d27b075738be9b14e084ac8698c
daxantz <daxantz@gmail.com>
2024-12-24 22:44:23 -0500

    refactor all jsx that displays weather to its own component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import WeatherData from "../../components/WeatherData";
 type Weather = {
   coord: {
     lon: number;
@@ -35,9 +36,6 @@ type Weather = {
   dt: number; // Data calculation time (UNIX timestamp)
 };
 
-function success(position) {
-  return position;
-}
 const Home: React.FC = () => {
   const [currentPosition, setCurrentPosition] =
     useState<GeolocationPosition | null>(null);
@@ -98,116 +96,7 @@ const Home: React.FC = () => {
   return (
     <div className="">
       {currentWeather ? (
-        <div className="flex">
-          <div className="border border-pink-400 w-[60%] ">
-            <h2>Home - Your Location</h2>
-            <div className="flex flex-col border border-blue-600 bg-[url('/cloudbg.png')] object-contain text-white bg-slate-700 p-7">
-              <div className="flex gap-3 mb-3">
-                <img className="w-14  " src="/clouds.png" alt="" />
-                <span>
-                  {currentWeather.weather[0]?.description || "No description"}
-                </span>
-              </div>
-
-              <span className="text-6xl mb-3 ">
-                {currentWeather.main?.temp
-                  ? `${Math.floor(currentWeather.main.temp)}°F`
-                  : "N/A"}
-              </span>
-              <span>{currentWeather.name || "Unknown location"}</span>
-            </div>
-            <p>Your Location</p>
-            <div className="flex w-full border border-purple-600 gap-5 ">
-              <div className="border border-red-600 w-48 flex flex-col items-center justify-center  bg-slate-600  text-white">
-                <h3>Feels Like</h3>
-                <span className="text-5xl font-medium">
-                  {currentWeather.main?.feels_like
-                    ? `${Math.floor(currentWeather.main.feels_like)}°F`
-                    : "N/A"}
-                </span>
-              </div>
-              <div className="border border-red-600 flex flex-col w-48  items-center justify-center  bg-slate-600  text-white">
-                <h3>Max Temp</h3>
-                <span className="text-5xl font-medium">
-                  {Math.floor(currentWeather.main.temp_max)}&deg;F
-                </span>
-                <span>{currentWeather.weather[0].main}</span>
-              </div>
-              <div className="border border-red-600 bg-slate-600  text-white  flex flex-col items-center justify-center w-48 ">
-                <h3>Min Temp</h3>
-                <span className="text-5xl font-medium ">
-                  {Math.floor(currentWeather.main.temp_min)}&deg;F
-                </span>
-                <span>{currentWeather.weather[0].main}</span>
-              </div>
-              <div className="border border-red-600 w-48 flex flex-col items-center justify-center  bg-slate-600  text-white">
-                <h3>Humidity</h3>
-                <span className="text-5xl font-medium">
-                  {currentWeather.main.humidity}%
-                </span>
-                <span>{currentWeather.weather[0].main}</span>
-              </div>
-            </div>
-          </div>
-          <div className=" w-full">
-            <div className="border  ">
-              <div className="p-10">
-                <img className="mb-4" src="/rain.png" alt="" />
-                <div className="flex justify-between mb-4">
-                  <h4>Rain</h4>
-                  <span>00 mm</span>
-                </div>
-                <p>
-                  The air quality is generally acceptable for most individuals.
-                  However, sensitive groups may experience minor to moderate
-                  symptoms from long-term exposure.
-                </p>
-              </div>
-            </div>
-            <div className=" ">
-              <div className="p-10">
-                <img className="mb-4" src="/wind.png" alt="" />
-                <div className="flex justify-between mb-4">
-                  <h4>Wind</h4>
-                  <span>2.06km/h</span>
-                </div>
-                <p>
-                  The air quality is generally acceptable for most individuals.
-                  However, sensitive groups may experience minor to moderate
-                  symptoms from long-term exposure.
-                </p>
-              </div>
-            </div>
-            <div>
-              <div className="p-10">
-                <img className="mb-4" src="/clouds.png" alt="" />
-                <div className="flex justify-between mb-4">
-                  <h4>Cloudy</h4>
-                  <span>95%</span>
-                </div>
-                <p>
-                  The air quality is generally acceptable for most individuals.
-                  However, sensitive groups may experience minor to moderate
-                  symptoms from long-term exposure.
-                </p>
-              </div>
-            </div>
-            <div>
-              <div className="p-10">
-                <img className="mb-4" src="/water.png" alt="" />
-                <div className="flex justify-between mb-4">
-                  <h4>Humidity</h4>
-                  <span>100%</span>
-                </div>
-                <p>
-                  The air quality is generally acceptable for most individuals.
-                  However, sensitive groups may experience minor to moderate
-                  symptoms from long-term exposure.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WeatherData currentWeather={currentWeather} />
       ) : (
         <p>Loading weather data...</p>
       )}
