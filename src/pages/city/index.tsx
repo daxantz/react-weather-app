2f89162240493984873ca8e1b620f6ba6c1d307c
daxantz <daxantz@gmail.com>
2024-12-29 13:20:27 -0500

    weatherData component now also accepts the first city in the cities array so the ui renders that city once any city item is delted

--- src/pages/city/index.tsx
+++ src/pages/city/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router";
 import Weather from "../../types/weather";
 import WeatherData from "../../components/WeatherData";
@@ -12,7 +12,7 @@ const City: React.FC<CityProps> = ({ cities }) => {
 
   const currentCity = cities.find((city) => city.id === Number(cityId));
 
-  return <WeatherData currentWeather={currentCity} />;
+  return <WeatherData currentWeather={currentCity || cities[0]} />;
 };
 
 export default City;
