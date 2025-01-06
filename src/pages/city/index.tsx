5c2fd193865014ebecc3fd77d79ebd01786b4c03
daxantz <daxantz@gmail.com>
2025-01-05 20:23:26 -0500

    city component now fetches data based off of the city name in the url params

--- src/pages/city/index.tsx
+++ src/pages/city/index.tsx
@@ -1,18 +1,46 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Weather from "../../types/weather";
 import WeatherData from "../../components/WeatherData";
-type CityProps = {
-  query: string;
-  setCities: React.Dispatch<React.SetStateAction<Weather[]>>;
-  cities: Weather[];
-};
-const City: React.FC<CityProps> = ({ cities }) => {
-  const { cityId } = useParams<{ cityId: string }>();
 
-  const currentCity = cities.find((city) => city.id === Number(cityId));
+const City = () => {
+  const { name } = useParams<{ name: string }>();
+  const [city, setCity] = useState<Weather | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchError, setFetchError] = useState<string | null>(null);
 
-  return <WeatherData currentWeather={currentCity || cities[0]} />;
+  useEffect(() => {
+    async function getWeather() {
+      try {
+        setIsLoading(true);
+
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}?q=${name}&appid=${
+            import.meta.env.VITE_API_KEY
+          }&units=imperial`
+        );
+        if (!res.ok) {
+          throw new Error(`Couldn't find that city`);
+        } else {
+          const data = (await res.json()) as Weather;
+          setCity(data);
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          setFetchError(error.message);
+        } else {
+          setFetchError("An unknown error occurred.");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getWeather();
+  }, [name]);
+  if (isLoading) return <p>Loading weather data...</p>;
+  if (fetchError) return <p>{fetchError}</p>;
+
+  return <WeatherData currentWeather={city} />;
 };
 
 export default City;
