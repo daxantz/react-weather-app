0dcb625b33218cc953999e813af4c679e4655aec
daxantz <daxantz@gmail.com>
2025-01-05 20:27:27 -0500

    move state into sidebar component remove sidebar props remove city component props and pass  it in as the only element to the route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,21 @@
-import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/home/index";
 import City from "./pages/city/index";
 import Header from "./components/Header";
-import Weather from "./types/weather";
-const App: React.FC = () => {
+
+const App = () => {
   // Use state for dynamic cities list
-  const [cities, setCities] = useState<Weather[]>([]);
-  const [query, setQuery] = useState<string>("");
 
   return (
     <Router>
       <div className="flex  ">
-        <Sidebar
-          setQuery={setQuery}
-          query={query}
-          cities={cities}
-          setCities={setCities}
-        />
+        <Sidebar />
         <div className="w-[75%] ">
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route
-              path="/city/:cityId"
-              element={
-                cities.length > 0 ? (
-                  <City query={query} setCities={setCities} cities={cities} />
-                ) : (
-                  <p>Search for a city to get started</p>
-                )
-              }
-            />
+            <Route path="/city/:name" element={<City />} />
           </Routes>
         </div>
       </div>
